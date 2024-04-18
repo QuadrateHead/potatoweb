e0e6f64b1d5a740fe255754e8d0dad337630e014
QuadrateHead <98948075+QuadrateHead@users.noreply.github.com>
2024-04-18 20:37:07 +0200

    Beta 1.0

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 let page = document.querySelector('.body');
 let button = document.querySelector('.themeButton');
+let news = document.querySelector('.news');
+var x = page.clientWidth;
 button.onclick = function(){
     page.classList.toggle('lightTheme')
     page.classList.toggle('darkTheme')
 }
+
+
+
+
+if ($(window).width() <= 400) {
+      $("#News").addClass("yesnews");
+      $("#News").removeClass("nonews");
+} 
+if ($(window).width() > 400) {
+      $("#News").removeClass("yesnews");
+      $("#News").addClass("nonews"); 
+}
+
+
+$('.yesnews').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    autoplay:true, 
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
